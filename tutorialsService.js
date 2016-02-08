03022812e01878295ea5b2a6f0d5a7fc07a1bc97
jfaucette <javonifaucette@hotmail.com>
2016-02-08 11:20:19 -0500

    Added app.service function

--- tutorialsService.js
+++ tutorialsService.js
@@ -1,2 +1,17 @@
 var app = angular.module("TutorialsApp", []);
 
+app.service("tutorialsService", function($http, $q)
+{
+  var deferred = $q.defer();
+  $http.get("tutorials.json").then(function (data)
+  {
+    deferred.resolve(data);
+  });
+
+  this.getTutorials = function ()
+  {
+    return deferred.promise;
+  }
+})
+
+
